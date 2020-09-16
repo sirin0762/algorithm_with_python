9d89629a88bd53c8c9e5316b1bfec996a64353ea
repl.it user <replituser@example.com>
2020-09-16 15:35:02 +0000

    2020-09-17(1)

--- algorithm with python/chapter 10. 그래프이론/10-9(커리큘럼).py
+++ algorithm with python/chapter 10. 그래프이론/10-9(커리큘럼).py
@@ -3,37 +3,39 @@
 from collections import deque
 import copy
 
-n = int(input())
+v = int(input())
 
-graph = [[] for i in range(n + 1)]
-time = [0] * (n + 1)
-indegree = [0] * (n + 1)
+graph = [[] for _ in range(v + 1)]
 
-for i in range(1, n + 1):
-    data = list(map(int, input().split()))
-    time[i] = data[0]
-    for x in data[1:-1]:
-        indegree[i] += 1
-        graph[x].append(i)
+times = [0] * (v + 1)
+indegree = [0] * (v + 1)
+
+for i in range(1, v + 1):
+  data = list(map(int, input().split()))
+  times[i] = data[0]
+  for j in data[1:-1]:
+    indegree[i] += 1
+    graph[j].append(i)
 
 def topology_sort():
-    result = copy.deepcopy(time)
-    q = deque()
+  q = deque()
+  result = copy.deepcopy(times)
+  for i in range(1, v + 1):
+    if indegree[i] == 0:
+      q.append(i)
+  
+  while q:
+    now = q.popleft()
 
-    for i in range(1, n + 1):
-        if indegree[i] == 0:
-            q.append(i)
+    for i in graph[now]:
+      result[i] = max(result[i], result[now] + times[i])
+      indegree[i] -= 1
 
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            result[i] = max(result[i], result[now] + time[i])
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
+      if indegree[i] == 0:
+        q.append(i)
 
-    for i in range(1, n + 1):
-        print(result[i])
+  for i in range(1, v + 1):
+    print(result[i])
 
 topology_sort()
 
